07334c3e12ad8aedc0521e509de3f566b64de5b6
Flavio Lucio <flaviolc18@gmail.com>
2018-07-26 15:26:53 -0300

    added template logic for ad search

--- work_project.py
+++ work_project.py
@@ -1,9 +1,21 @@
-import flask
+from flask import (Flask, render_template, request)
 
-app = flask.Flask("work_project")
+app = Flask("work_project")
 
 @app.route('/')
 def index():
-  return flask.render_template("index.html", name="test user")
+  return render_template("main.html", name="test user")
+
+#try only with the post
+@app.route('/handlead', methods=["GET", "POST"])
+def handlead():
+  if request.method == "POST":
+    comp = request.form['comp']
+    serv = request.form['serv']
+    if comp and serv:
+      return render_template("foundads.html", comp=comp, serv=serv)
+    else:
+      return render_template("main.html", error_msg="Fill all the the form")
+
 
 app.run(debug=True, use_reloader=True)

90130f6936acbd68715e35324ad9eec9d674d6c2
Flavio Lucio <flaviolc18@gmail.com>
2018-08-09 20:01:26 -0300

    db working

--- blueprints/ads.py
+++ blueprints/ads.py
@@ -1,17 +1,27 @@
 from flask import (Blueprint, render_template, request)
 
-from db import ads_table
+from db import get_table
 
 ads_blueprint = Blueprint('ads', __name__, template_folder='templates')
 
 @ads_blueprint.route("/ads/create", methods=["GET", "POST"])
 def create():
   if request.method == "POST":
-    return render_template('ad_created.html', title=u"Ad created successfully")
+    ads_table = get_table('ads') 
+    
+    data = request.form.to_dict()
+
+    id_new_ad = ads_table.insert(data)
+
+    return render_template('ad_created.html', title=u"Ad created successfully", id_new_ad=id_new_ad)
   else:
     return render_template('create_ad.html', title=u"Create a new Ad")
 
 @ads_blueprint.route("/")
 def index():
-  #all_ads = ads_table.all()
-  return render_template('index.html', title=u"All ads") 
+  ads_table = get_table('ads') 
+
+  if(ads_table):
+    return render_template('index.html', title=u"All ads", ads=ads_table)
+  else:
+    return render_template('index.html', title=u"All ads")
--- db.py
+++ db.py
@@ -1,4 +1,9 @@
+from os import path
+from flask import current_app
 import dataset
 
-db = dataset.connect('sqlite:///worktool.db')
-ads_table = db['worktool']
+def get_table(tablename):
+  db_name = current_app.config['DATABASE_NAME']
+  db_path = path.join(current_app.config['PROJECT_ROOT'], db_name)
+  db = dataset.connect('sqlite:///{0}'.format(db_path))
+  return db[tablename]

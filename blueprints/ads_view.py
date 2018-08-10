d43444f436620eeef419d0ec81fb468967f2ee83
Flavio Lucio <flaviolc18@gmail.com>
2018-08-10 16:43:19 -0300

    session num_ads added

--- blueprints/ads_view.py
+++ blueprints/ads_view.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template, request, flash, redirect, url_for)
+from flask import (Blueprint, render_template, request, flash, redirect, url_for, make_response, session)
 from db import db
 from blueprints.ads_model import Ad, Rate
 
@@ -29,7 +29,23 @@ def new():
   else:
     return render_template('new_ad.html', title=u"Create a new Ad")
 
+@ads_blueprint.route("/ads/show_all")
+def show_all():
+  all_ads = Ad.query.all()
+  if all_ads:
+    return render_template('show_all_ads.html', all_ads=all_ads)
+  else:
+    return render_template('show_all_ads.html', error_msg="No ads to show...")
+
+@ads_blueprint.route("/ads", methods=["POST"])
+def handler():
+  if(request.method == "POST"):
+
+    if(request.form['num_ads']):
+      session['num_ads'] = request.form['num_ads']
+
+    return render_template('find_ads.html')
+
 @ads_blueprint.route("/")
 def index():
-  all_ads = Ad.query.all()
-  return render_template('index.html', all_ads=all_ads)
+  return render_template('index.html')

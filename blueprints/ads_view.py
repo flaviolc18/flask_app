fbfb1f109a565eca74fa5f6053a962d69856491e
Flavio Lucio <flaviolc18@gmail.com>
2018-08-11 19:54:49 -0300

    added next function

--- blueprints/ads_view.py
+++ blueprints/ads_view.py
@@ -6,13 +6,16 @@ from bot.bot import Bot
 #create a blueprint
 ads_blueprint = Blueprint('ads', __name__, template_folder='templates')
 
-@ads_blueprint.route("/ads", methods=["POST"])
+@ads_blueprint.route("/ads", methods=["POST", "GET"])
 def handler():
   if(request.method == "POST"):
 
-    if(request.form['num_ads']):
+    if("num_ads" in request.form):
       session['num_ads'] = request.form['num_ads']
+      session['num_made_ads'] = 0
 
+    return render_template('find_ads.html')
+  else:
     return render_template('find_ads.html')
 
 @ads_blueprint.route("/ads/new", methods=["GET", "POST"])
@@ -36,22 +39,24 @@ def new():
 
     return redirect(url_for('ads.index'))
   else:
-    return render_template('new_ad.html', title=u"Create a new Ad")
+    return render_template('new_ad.html')
 
 @ads_blueprint.route("/ads/show_all")
 def show_all():
   if(request.args.get('option') == "get_ads"):
 
     all_ads = Ad.query.filter(Ad.company.like("%"+request.args.get('comp')+"%"), Ad.service.like("%"+request.args.get('serv')+"%"), Rate.comment.like("%"+request.args.get('key')+"%")).all()
+
+    session["comp"] = request.args.get('comp')
+    session["serv"] = request.args.get('serv')
+
     return jsonify([a.serialize() for a in all_ads])
   else:
 
     all_ads = Ad.query.all()
     if all_ads:
-
       return render_template('show_all_ads.html', all_ads=all_ads)
     else:
-
       return render_template('show_all_ads.html', error_msg="No ads to show...")
 
 @ads_blueprint.route("/ads/show")
@@ -62,14 +67,26 @@ def show():
 
     bot = Bot()
     if(bot.get_ready()):
+
       bot.send_msg(ad.rate.comment)
       flash("Message sent successfully!")
+      session['num_made_ads'] += 1
     else:
       flash("Fail send message, start the bot in the Telegram chat...")
 
+    #here I must show all ad properties
     return render_template("show_ad.html", id_ad_selected=request.args['ad_id'])
   else:
     return render_template("show_ad.html")
+
+@ads_blueprint.route("/ads/next")
+def next():
+  session['comp'] = ""
+  session['serv'] = ""
+  #setar depois no login para nao ter possibilidade de acesso sem estar setada
+  session['num_made_ads'] += 1
+  return redirect(url_for('ads.handler'))
+
 
 @ads_blueprint.route("/")
 def index():

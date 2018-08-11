3d5a23ef2073f5da043a28c3ac0c3fbad9c0e7f3
Flavio Lucio <flaviolc18@gmail.com>
2018-08-11 18:01:13 -0300

    added bot class to send msg

---
+++ bot/bot.py
@@ -0,0 +1,23 @@
+import telepot
+
+class Bot:
+  _bot = None
+  _chat_id = None
+
+  __init__(self):
+  self.bot = telepot.Bot("647490139:AAGHiZzb06ZjbYnnmwJrslBRm1B_MrrUhXc")
+  
+  def get_ready(chat_id=None):
+    if chat_id:
+      self.chat_id = chat_id
+      return True
+    else:
+      resp = self.bot.getUpdates()
+      if not resp:
+        return False
+      else:
+        self.chat_id = resp[0]['message']['from']['id']
+        return True
+
+  def send_msg(self, msg):
+    self.bot.sendMessage(self.chat_id, msg)
